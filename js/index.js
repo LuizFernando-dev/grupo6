99a07ecb08615a4f0156e3d686965a02d37b251e
unknown <luizferbarbosaap@gmail.com>
2020-09-09 15:09:00 -0300

    função de ordenação da tabela qualitativa ordinal

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
-var ObjetoDado = function(dado, quant){
+var objetosOrdinal = [];
+let indiceTabelaGlobal = 0;
+
+var ObjetoDado = function(dado, quant, indice){
 		this.dado = dado;
 		this.quantidade = quant;
+		this.indice = indice;
 	}
-
 
 function calcular() {
 	var variavel = document.getElementById("variavel");
@@ -17,19 +20,22 @@ function calcular() {
 	var arrayObjetos = [];
 	arrayObjetos = criaObjetos(dadosArray);
 
-	if(tipo.value == "ordinal"){
-		tabelaOrdinal(arrayObjetos,variavel)
+	if(tipo.value == "continua"){
+		tabelaContinua(arrayObjetos)
 	}else{
 		var objetos = [];
 		objetos = quantidade(arrayObjetos);
-
-		objetos = mudaOrdem(tipo, objetos);
-
-		criaTabela(variavel, objetos);
-
-		for(var i = 0; i <= objetos.length; i++){
-			console.log(objetos[i]);
+		if(tipo.value == "ordinal"){
+			objetos = arrumaIndice(objetos)
+			objetosOrdinal = objetos
+			tabelaOrdinal()
+		}else{
+			objetos = mudaOrdem(tipo, objetos);
+			criaTabela(objetos);
 		}
+
+		objetosOrdinal = objetos;
+
 	}
 	
 }
@@ -37,7 +43,7 @@ function calcular() {
 function criaObjetos(dados){
 	var objetos = [];
 	for(var i = 0; i <= dados.length; i++){
-		objetos.push(new ObjetoDado(dados[i], 1));
+		objetos.push(new ObjetoDado(dados[i], 1, i));
 	}
 
 	return objetos;
@@ -78,10 +84,9 @@ function quantidade(objts){
 	return objts2;
 }
 
-function criaTabela(variavel, array){
-	var nomeVariavel = document.getElementById("nomeVariavel");
-	nomeVariavel.innerHTML = variavel.value;
-	var tabela = document.getElementById("tabela");
+function criaTabela(array){
+	var tabela = criaTabelaOriginal();
+	let nomeVariavel = mudaNomeVariavel();
 	tabela.setAttribute("style", "display: ;");
 
 	for(var i = 0; i < array.length; i++){
@@ -110,7 +115,7 @@ function mudaOrdem(ordem,array){
 }
 
 
-function tabelaOrdinal(array,variavel){
+function tabelaContinua(array){
 	let maior = 0
 	let menor = 99999
 	array.pop()
@@ -148,10 +153,10 @@ function tabelaOrdinal(array,variavel){
 		linha = linha3
 	}
 	
-	var nomeVariavel = document.getElementById("nomeVariavel");
-	nomeVariavel.innerHTML = variavel.value;
-	var tabela = document.getElementById("tabela");
+	var tabela = criaTabelaOriginal()
+	let nomeVariavel = mudaNomeVariavel();
 	tabela.setAttribute("style", "display: ;");
+
 	let intervalo2 = intervalo
 	let valor1 = menor
 	for(var i = 1; i <= linha; i++){
@@ -180,3 +185,139 @@ function tabelaOrdinal(array,variavel){
 	}
 
 }
+
+function tabelaOrdinal(){
+	var tabela = criaTabelaOriginal()
+	tabela.setAttribute("style", "display: ;")
+	let nomeVariavel = mudaNomeVariavel();
+	var tdPosicao = document.createElement("td")
+	tdPosicao.innerHTML = "Posição";
+	var tabelaCorpo = document.getElementById("tabelaCorpo"+indiceTabelaGlobal)
+	tabelaCorpo.appendChild(tdPosicao)
+
+	for(var i = 0; i < objetosOrdinal.length; i++){
+		var tr = document.createElement("tr")
+		var td = document.createElement("td")
+		var tdd = document.createElement("td")
+
+		var input = document.createElement("input")
+		input.setAttribute("type", "number")
+		input.setAttribute("style", "width : 30px")
+		input.setAttribute("id", "pos"+indiceTabelaGlobal + i)
+
+		td.innerHTML = objetosOrdinal[i].dado
+		tdd.innerHTML = objetosOrdinal[i].quantidade
+		input.value = objetosOrdinal[i].indice + 1
+		
+		
+		tr.appendChild(td)
+
+		tr.appendChild(tdd)
+		tr.appendChild(input)
+
+		tabela.appendChild(tr)
+	}
+
+	let button = document.createElement("button")
+	button.setAttribute("Onclick", "ordenando()")
+	button.innerHTML = "Ordenar"
+	tabela.appendChild(button)
+}
+
+function ordenando(){
+	for(var i = 0; i < objetosOrdinal.length; i++){
+		var valor = document.getElementById("pos"+indiceTabelaGlobal+ i)
+		objetosOrdinal[i].indice = valor.value
+	}
+
+	objetosOrdinal.sort((a, b) => a.indice - b.indice)
+
+	for(var i = 0; i < objetosOrdinal.length; i++){
+		objetosOrdinal[i].indice = i 
+	} 
+
+	tabelaOrdinal()
+
+} 
+
+/*function ordenando(){
+	var array = []
+	var pos = -1
+	var destino = -1
+	for(var i = 0; i < objetosOrdinal.length; i++){
+		var valor = document.getElementById("pos"+i)
+		array.push(valor.value)
+		if(array[i] != i){
+			pos = i
+			destino = array[i]
+		}
+	}
+
+	if(pos >= 0){
+		objetosOrdinal.splice(destino,0,objetosOrdinal[pos])
+		delete objetosOrdinal[pos+1]	
+	}
+	
+	for(var i = 0; i < objetosOrdinal.length; i++){
+		if(typeof objetosOrdinal[i] === 'undefined'){
+			objetosOrdinal.splice(i,1)
+		}
+	}
+
+	for(var i = 0; i < objetosOrdinal.length; i++){
+		console.log(objetosOrdinal[i])
+	}
+
+	console.log("-----------------")
+
+	tabelaOrdinal(objetosOrdinal)
+
+} */
+
+function mudaNomeVariavel(){
+	var nomeVariavel = document.getElementById("nomeVariavel"+indiceTabelaGlobal);
+	nomeVariavel.innerHTML = variavel.value;
+	return nomeVariavel;
+}
+
+function criaTabelaOriginal(){
+	var indice = document.getElementById("indice")
+	var indiceTabela = indice.value
+	if(indice.value > 0){
+		for(var i = 0; i < indice.value; i++){
+			document.getElementById("tabela"+i).setAttribute("style","display:none")
+		}
+	}
+
+	indice.value = Number(indice.value) + 1
+	indiceTabelaGlobal = indiceTabela
+	var table = document.createElement("table")
+	table.setAttribute("style","display:none")
+	table.setAttribute("id","tabela"+indiceTabela)
+	var tr = document.createElement("tr")
+	var td = document.createElement("td")
+	td.setAttribute("id", "nomeVariavel"+indiceTabela)
+	var tdd = document.createElement("td")
+	td.innerHTML = variavel;
+	tdd.innerHTML = "Frequência"
+	tr.setAttribute("id", "tabelaCorpo"+indiceTabela)
+
+	tr.appendChild(td)
+	tr.appendChild(tdd)
+
+	table.appendChild(tr)
+
+	var corpo = document.getElementById("tabelas")
+	corpo.appendChild(table)
+
+	return document.getElementById("tabela"+indiceTabela)
+}
+
+function arrumaIndice(array){
+	for(var i = 0; i < array.length; i++){
+		array[i].indice = i
+	}
+
+	return array
+}
+
