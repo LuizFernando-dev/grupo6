6125bdbbc70e5f026a5dea7af45bdef971ea8e09
unknown <luizferbarbosaap@gmail.com>
2020-09-12 19:08:59 -0300

    implementação das frequencias

--- js/index.js
+++ js/index.js
@@ -1,72 +1,72 @@
-var objetosOrdinal = [];
-let indiceTabelaGlobal = 0;
+var objetosOrdinal = []
+var indiceTabelaGlobal = 0
 
 var ObjetoDado = function(dado, quant, indice){
-		this.dado = dado;
-		this.quantidade = quant;
-		this.indice = indice;
+		this.dado = dado
+		this.quantidade = quant
+		this.indice = indice
+		this.frequenciaPorcentagem = 0
+		this.freqAcumulada = 0
+		this.acumuladaP = 0
 	}
 
 function calcular() {
-	var variavel = document.getElementById("variavel");
-	var tipo = document.getElementById("tipo");
+	const variavel = document.getElementById("variavel")
+	let tipo = document.getElementById("tipo")
 
-	var dadosArray = [];
-	var dados = document.querySelector("#dados");
-	var valores = dados.value;
+	const dados = document.querySelector("#dados")
+	const valores = dados.value
 
-	dadosArray = valores.split(",");
+	const dadosArray = valores.split(",")
 
-	var arrayObjetos = [];
-	arrayObjetos = criaObjetos(dadosArray);
+	let arrayObjetos = criaObjetos(dadosArray)
 
 	if(tipo.value == "continua"){
 		tabelaContinua(arrayObjetos)
 	}else{
-		var objetos = [];
-		objetos = quantidade(arrayObjetos);
+		let objetos = quantidade(arrayObjetos)
+		objetos = Frequencias(objetos)
 		if(tipo.value == "ordinal"){
 			objetos = arrumaIndice(objetos)
 			objetosOrdinal = objetos
 			tabelaOrdinal()
 		}else{
-			objetos = mudaOrdem(tipo, objetos);
-			criaTabela(objetos);
+			objetos = mudaOrdem(tipo, objetos)
+			criaTabela(objetos)
 		}
 
-		objetosOrdinal = objetos;
-
+		objetosOrdinal = objetos
 	}
 	
 }
 
 function criaObjetos(dados){
-	var objetos = [];
-	for(var i = 0; i <= dados.length; i++){
-		objetos.push(new ObjetoDado(dados[i], 1, i));
+	let objetos = []
+	for(let i = 0; i <= dados.length; i++){
+		objetos.push(new ObjetoDado(dados[i], 1, i))
 	}
 
-	return objetos;
+	return objetos
 
 }
 
 function quantidade(objts){
-	let excluir = 0;
-	var cont = 0;
-	var objts2 = [];
-	for(var i = 0; i < objts.length; i++){
-		cont++;
-		let boolean = true;
-		for(var j = cont; j < objts.length; j++){
+	let cont = 0
+	let objts2 = []
+
+	for(let i = 0; i < objts.length; i++){
+		cont++
+		let boolean = true
+		for(let j = cont; j < objts.length; j++){
 			if(objts[i].dado == objts[j].dado ){
-				objts[i].quantidade = objts[i].quantidade + 1;
+				objts[i].quantidade = objts[i].quantidade + 1
 			}
 		}
 
 		if(objts2.length != 0){
-			for(var n = 0; n < objts2.length; n++){
+			for(let n = 0; n < objts2.length; n++){
 				if(objts2[n].dado == objts[i].dado){
-					boolean = false;
+					boolean = false
 				}
 			}	
 
@@ -81,26 +81,36 @@ function quantidade(objts){
 
 	objts = objts2
 	
-	return objts2;
+	return objts2
 }
 
 function criaTabela(array){
-	var tabela = criaTabelaOriginal();
-	let nomeVariavel = mudaNomeVariavel();
-	tabela.setAttribute("style", "display: ;");
+	let tabela = criaTabelaOriginal()
+	let nomeVariavel = mudaNomeVariavel()
+	tabela.setAttribute("style", "display: ;")
 
-	for(var i = 0; i < array.length; i++){
-		var tr = document.createElement("tr");
-		var td = document.createElement("td");
-		var tdd = document.createElement("td");
+	for(let i = 0; i < array.length; i++){
+		let tr = document.createElement("tr")
+		let td = document.createElement("td")
+		let tdd = document.createElement("td")
+		let tdFP = document.createElement("td")
+		let freqAcumulada = document.createElement("td")
+		let acumuladaP = document.createElement("td")
 
-		td.innerHTML = array[i].dado;
-		tdd.innerHTML = array[i].quantidade;
+		td.innerHTML = array[i].dado
+		tdd.innerHTML = array[i].quantidade
+		tdFP.innerHTML = array[i].frequenciaPorcentagem.toFixed(1)
+		freqAcumulada.innerHTML = array[i].freqAcumulada
+		acumuladaP.innerHTML = array[i].acumuladaP
+		
+	
+		tr.appendChild(td)
+		tr.appendChild(tdd)
+		tr.appendChild(tdFP)
+		tr.appendChild(freqAcumulada)
+		tr.appendChild(acumuladaP)
 
-		tr.appendChild(td);
-		tr.appendChild(tdd);
-
-		tabela.appendChild(tr);
+		tabela.appendChild(tr)
 	}
 }
 
@@ -110,7 +120,7 @@ function mudaOrdem(ordem,array){
 		array.sort((a, b) => a.dado - b.dado)
 	}
 
-	return array;
+	return array
 	
 }
 
@@ -118,8 +128,9 @@ function mudaOrdem(ordem,array){
 function tabelaContinua(array){
 	let maior = 0
 	let menor = 99999
+	let frequenciaAcumulada = 0
 	array.pop()
-	for(var i = 0; i < array.length; i++){
+	for(let i = 0; i < array.length; i++){
 		if(array[i].dado > maior){
 			maior = array[i].dado
 		}
@@ -129,7 +140,7 @@ function tabelaContinua(array){
 		}
 	}
 
-	var amplitude = maior - menor
+	let amplitude = maior - menor
 	amplitude += 1
 
 	let linha1 = Math.sqrt(array.length).toFixed(0)
@@ -153,54 +164,73 @@ function tabelaContinua(array){
 		linha = linha3
 	}
 	
-	var tabela = criaTabelaOriginal()
-	let nomeVariavel = mudaNomeVariavel();
-	tabela.setAttribute("style", "display: ;");
+	let tabela = criaTabelaOriginal()
+	let nomeVariavel = mudaNomeVariavel()
+	tabela.setAttribute("style", "display: ;")
 
 	let intervalo2 = intervalo
 	let valor1 = menor
-	for(var i = 1; i <= linha; i++){
-		var tr = document.createElement("tr");
-		var td = document.createElement("td");
-		var tdd = document.createElement("td");
+	for(let i = 1; i <= linha; i++){
+		let tr = document.createElement("tr")
+		let td = document.createElement("td")
+		let tdd = document.createElement("td")
+		let tdFP = document.createElement("td")
+		let freqAcumulada = document.createElement("td")
+		let acumuladaP = document.createElement("td")
 
 		let valor2 = Number(intervalo) + Number(menor)
 		intervalo += intervalo2
 
-		let frequencia = 0;
-		for(var j = 0; j < array.length; j++){
+		let frequencia = 0
+		for(let j = 0; j < array.length; j++){
 			if(array[j].dado >= valor1 && array[j].dado < valor2){
-				frequencia++;
+				frequencia++
 			}
 		}
 
-		td.innerHTML = valor1 + "-" + valor2
-		tdd.innerHTML = frequencia;
+		let total = 0
+		for(let i = 0; i < array.length; i++){
+			total += array[i].quantidade
+		}
+
+		frequenciaAcumulada += frequencia
+
+		td.innerHTML = valor1 + "|--" + valor2
+		tdd.innerHTML = frequencia
 		valor1 = valor2
+		tdFP.innerHTML = ((frequencia * 100)/total).toFixed(1)
+		freqAcumulada.innerHTML = frequenciaAcumulada
+		acumuladaP.innerHTML = (frequenciaAcumulada*100)/total
 
-		tr.appendChild(td);
-		tr.appendChild(tdd);
+		tr.appendChild(td)
+		tr.appendChild(tdd)
+		tr.appendChild(tdFP)
+		tr.appendChild(freqAcumulada)
+		tr.appendChild(acumuladaP)
 
-		tabela.appendChild(tr);
+		tabela.appendChild(tr)
 	}
 
 }
 
 function tabelaOrdinal(){
-	var tabela = criaTabelaOriginal()
+	let tabela = criaTabelaOriginal()
 	tabela.setAttribute("style", "display: ;")
 	let nomeVariavel = mudaNomeVariavel();
-	var tdPosicao = document.createElement("td")
-	tdPosicao.innerHTML = "Posição";
-	var tabelaCorpo = document.getElementById("tabelaCorpo"+indiceTabelaGlobal)
+	let tdPosicao = document.createElement("td")
+	tdPosicao.innerHTML = "Posição"
+	let tabelaCorpo = document.getElementById("tabelaCorpo"+indiceTabelaGlobal)
 	tabelaCorpo.appendChild(tdPosicao)
 
-	for(var i = 0; i < objetosOrdinal.length; i++){
-		var tr = document.createElement("tr")
-		var td = document.createElement("td")
-		var tdd = document.createElement("td")
+	for(let i = 0; i < objetosOrdinal.length; i++){
+		let tr = document.createElement("tr")
+		let td = document.createElement("td")
+		let tdd = document.createElement("td")
+		let tdFP = document.createElement("td")
+		let freqAcumulada = document.createElement("td")
+		let acumuladaP = document.createElement("td")
 
-		var input = document.createElement("input")
+		let input = document.createElement("input")
 		input.setAttribute("type", "number")
 		input.setAttribute("style", "width : 30px")
 		input.setAttribute("id", "pos"+indiceTabelaGlobal + i)
@@ -208,11 +238,15 @@ function tabelaOrdinal(){
 		td.innerHTML = objetosOrdinal[i].dado
 		tdd.innerHTML = objetosOrdinal[i].quantidade
 		input.value = objetosOrdinal[i].indice + 1
-		
+		tdFP.innerHTML = objetosOrdinal[i].frequenciaPorcentagem.toFixed(1)
+		freqAcumulada.innerHTML = objetosOrdinal[i].freqAcumulada
+		acumuladaP.innerHTML = objetosOrdinal[i].acumuladaP
 		
 		tr.appendChild(td)
-
 		tr.appendChild(tdd)
+		tr.appendChild(tdFP)
+		tr.appendChild(freqAcumulada)
+		tr.appendChild(acumuladaP)
 		tr.appendChild(input)
 
 		tabela.appendChild(tr)
@@ -225,97 +259,96 @@ function tabelaOrdinal(){
 }
 
 function ordenando(){
-	for(var i = 0; i < objetosOrdinal.length; i++){
-		var valor = document.getElementById("pos"+indiceTabelaGlobal+ i)
+	for(let i = 0; i < objetosOrdinal.length; i++){
+		let valor = document.getElementById("pos"+indiceTabelaGlobal+ i)
 		objetosOrdinal[i].indice = valor.value
 	}
 
 	objetosOrdinal.sort((a, b) => a.indice - b.indice)
 
-	for(var i = 0; i < objetosOrdinal.length; i++){
+	for(let i = 0; i < objetosOrdinal.length; i++){
 		objetosOrdinal[i].indice = i 
 	} 
 
+	Frequencias(objetosOrdinal)
 	tabelaOrdinal()
 
 } 
 
-/*function ordenando(){
-	var array = []
-	var pos = -1
-	var destino = -1
-	for(var i = 0; i < objetosOrdinal.length; i++){
-		var valor = document.getElementById("pos"+i)
-		array.push(valor.value)
-		if(array[i] != i){
-			pos = i
-			destino = array[i]
-		}
-	}
-
-	if(pos >= 0){
-		objetosOrdinal.splice(destino,0,objetosOrdinal[pos])
-		delete objetosOrdinal[pos+1]	
-	}
-	
-	for(var i = 0; i < objetosOrdinal.length; i++){
-		if(typeof objetosOrdinal[i] === 'undefined'){
-			objetosOrdinal.splice(i,1)
-		}
-	}
-
-	for(var i = 0; i < objetosOrdinal.length; i++){
-		console.log(objetosOrdinal[i])
-	}
-
-	console.log("-----------------")
-
-	tabelaOrdinal(objetosOrdinal)
-
-} */
 
 function mudaNomeVariavel(){
-	var nomeVariavel = document.getElementById("nomeVariavel"+indiceTabelaGlobal);
-	nomeVariavel.innerHTML = variavel.value;
-	return nomeVariavel;
+	let nomeVariavel = document.getElementById("nomeVariavel"+indiceTabelaGlobal)
+	nomeVariavel.innerHTML = variavel.value
+	return nomeVariavel
 }
 
 function criaTabelaOriginal(){
-	var indice = document.getElementById("indice")
-	var indiceTabela = indice.value
+	let indice = document.getElementById("indice")
+	let indiceTabela = indice.value
 	if(indice.value > 0){
-		for(var i = 0; i < indice.value; i++){
+		for(let i = 0; i < indice.value; i++){
 			document.getElementById("tabela"+i).setAttribute("style","display:none")
 		}
 	}
 
 	indice.value = Number(indice.value) + 1
 	indiceTabelaGlobal = indiceTabela
-	var table = document.createElement("table")
+	let table = document.createElement("table")
 	table.setAttribute("style","display:none")
 	table.setAttribute("id","tabela"+indiceTabela)
-	var tr = document.createElement("tr")
-	var td = document.createElement("td")
+	let tr = document.createElement("tr")
+	let td = document.createElement("td")
 	td.setAttribute("id", "nomeVariavel"+indiceTabela)
-	var tdd = document.createElement("td")
+	let tdd = document.createElement("td")
+	let tdFP = document.createElement("td")
+	let tdAcumulada = document.createElement("td")
+	let tdAcumuladaP = document.createElement("td")
 	td.innerHTML = variavel;
+	tdFP.innerHTML = "Frequência %"
 	tdd.innerHTML = "Frequência"
+	tdAcumulada.innerHTML = "F. Acumulada"
+	tdAcumuladaP.innerHTML = "Acumulada %"
 	tr.setAttribute("id", "tabelaCorpo"+indiceTabela)
 
 	tr.appendChild(td)
 	tr.appendChild(tdd)
+	tr.appendChild(tdFP)
+	tr.appendChild(tdAcumulada)
+	tr.appendChild(tdAcumuladaP)
 
 	table.appendChild(tr)
 
-	var corpo = document.getElementById("tabelas")
+	let corpo = document.getElementById("tabelas")
 	corpo.appendChild(table)
 
 	return document.getElementById("tabela"+indiceTabela)
 }
 
 function arrumaIndice(array){
-	for(var i = 0; i < array.length; i++){
+	for(let i = 0; i < array.length; i++){
 		array[i].indice = i
+	}
+
+	return array
+}
+
+
+function Frequencias(array){
+	let quantidade = 0
+
+	for(let i = 0; i < array.length; i++){
+		quantidade += array[i].quantidade
+	}
+
+	for(let i = 0; i < array.length; i++){
+		array[i].frequenciaPorcentagem = (array[i].quantidade * 100)/quantidade
+		if(i > 0){
+			array[i].freqAcumulada = array[i-1].freqAcumulada + array[i].quantidade
+		}else{
+			array[i].freqAcumulada = array[i].quantidade
+		}
+
+		array[i].acumuladaP = (array[i].freqAcumulada*100)/quantidade
 	}
 
 	return array
